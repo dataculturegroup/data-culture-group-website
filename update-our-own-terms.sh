76b0fbb3883c49dad5fd722173fa0f8aed36ee5a
Rahul Bhargava <rahulbot@gmail.com>
2026-05-06 15:00:30 -0400

    more work on CI for submodules

--- update-our-own-terms.sh
+++ update-our-own-terms.sh
@@ -6,7 +6,7 @@ SUBMODULE_PATH="our-own-terms"
 
 echo "Updating git submodule: $SUBMODULE_PATH"
 
-git submodule update --remote --merge "$SUBMODULE_PATH"
+git submodule update --remote --checkout "$SUBMODULE_PATH"
 
 echo "Staging submodule update..."
 git add "$SUBMODULE_PATH"
